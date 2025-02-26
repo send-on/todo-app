d9321bdfc9c17e1a236943d8b8e268f2c607bd39
Nicolas Emery <Nicolas5150@gmail.com>
2025-02-25 17:16:39 -0800

    Refactor to base todo app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,82 @@
-// Exercise 1:
-// Add todo list functionality to the frontend (Add and Delete) 
-// Style the todo list to look similar to the following: https://imgur.com/a/dNiZhTU
-
-import React from 'react';   
-const App = () => { 
-    return ( 
-      <div >
-        <div >
-          To-DO List
-        </div>
-          <div >
-            <input
-              className='border border-gray-400 p-2'
-              type='text'
-            />
-            <button
-              className='bg-blue-400 p-2 text-white'
-              onClick={()=>console.log('added')}
-            >
-              Add
-            </button>
-          </div>
-          <div>
-          <ul>
-            <Item />
-            <Item />
-            <Item />
-          </ul>
-          </div>
-          <div className=''>
-            <button
-              className='bg-blue-400 p-2 text-white'
-              > Get Weather</button>
-          </div>
-          <div className='text-black'>
-            Hows the weather?
-          </div>
-      </div>
-    ); 
-}; 
-  
-export default App;
+import React from 'react';
 
 const Item = () => (
-  <div >
-    <div >Item Name</div>
-    <div>
-      <button 
-        onClick={() => console.log('deleted')}
-        className='bg-red-400 p-2 text-white'>Delete</button>
-    </div>
+  <div style={{
+    border: 'solid black 1px'
+  }}>
+    <div>Item</div>
+    <button
+      type="button"
+      onClick={() => { console.log('Delete') }}
+      style={{ backgroundColor: 'red', padding: '10px', color: 'white' }}
+    >
+      Delete
+    </button>
   </div>
 )
+
+const App = () => {
+  // For Next.js reference, you can use the following as the urls:
+  const weatherURL = '/api/getWeather';
+  const webhookUrl = '/api/sendData';
+
+  const handleClick = async () => {
+    console.log('Get Weather');
+  }
+
+  return (
+    <div style={{ color: 'black' }}>
+
+      <div>
+        To-Do List
+      </div>
+
+      <div >
+        <input
+          aria-label='input'
+          style={{
+            border: '1px solid black',
+            padding: '8px'
+          }}
+          type='text'
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <button
+          type="button"
+          style={{
+            backgroundColor: 'blue',
+            color: 'white',
+            marginLeft: '4px',
+            padding: '9px',
+          }}
+          onClick={() => console.log('Add')}
+        >
+          Add
+        </button>
+      </div>
+
+      <Item />
+      <Item />
+      <Item />
+
+      <button
+        type="button"
+        style={{
+          color: 'white',
+          backgroundColor: 'blue',
+          padding: '10px',
+        }}
+        onClick={handleClick}
+      >
+        Get Weather
+      </button>
+
+      <div>
+        Hows the weather?
+      </div>
+
+    </div >
+  );
+};
+
+export default App;
